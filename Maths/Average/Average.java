b53ceed35da4515baa0f8249bd7820b0da326dc6
Gian <gianbdeveloper@hotmail.com>
2023-06-23 17:47:02 -0300

    Average Algorithm

---
+++ Maths/Average/Average.java
@@ -0,0 +1,24 @@
+package Average;
+
+/**
+ * Calculate average of a list of numbers
+ */
+public class Average {
+
+    /**
+     * Calculate average of a list of numbers
+     *
+     * @param numbers array to store numbers
+     * @return mean of given numbers
+     */
+    public static double average(double[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            throw new IllegalArgumentException("Numbers array cannot be empty or null");
+        }
+        double sum = 0;
+        for (double number : numbers) {
+            sum += number;
+        }
+        return sum / numbers.length;
+    }
+}
