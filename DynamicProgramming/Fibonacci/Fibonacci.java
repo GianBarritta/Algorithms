54bba3741d08485e5533e7c98ef2ce6bc5b84fbb
Gian <gianbdeveloper@hotmail.com>
2023-06-28 20:31:03 -0300

    Fibonacci algorithm

---
+++ DynamicProgramming/Fibonacci/Fibonacci.java
@@ -0,0 +1,27 @@
+package Fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        int a = 0;
+        int b = 1;
+        int c;
+        int n;
+
+        System.out.println("Please enter the number of elements for the series: ");
+        n = keyboard.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(a);
+            c = a + b;
+            a = b;
+            b = c;
+
+        }
+
+    }
+}
